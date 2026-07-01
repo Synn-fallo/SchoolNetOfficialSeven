d52e0bb6a353077ae849288c5d51c1d8fd63b497
Synn-fallo <145714970+Synn-fallo@users.noreply.github.com>
2026-07-02 00:28:18 +0100

    Create EnseignantMesClasses component

---
+++ src/pages/EnseignantMesClasses.tsx
@@ -0,0 +1,9 @@
+// /src/pages/EnseignantMesClasses.tsx
+// Page "Mes classes"
+
+import React from 'react';
+import MesClassesView from '@/components/enseignant/MesClassesView';
+
+export default function EnseignantMesClasses() {
+  return <MesClassesView />;
+}
