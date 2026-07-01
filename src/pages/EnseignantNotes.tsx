12b8a7af7a29177f36da2659bf41bee120827343
Synn-fallo <145714970+Synn-fallo@users.noreply.github.com>
2026-07-02 00:29:09 +0100

    Add EnseignantNotes page component

---
+++ src/pages/EnseignantNotes.tsx
@@ -0,0 +1,9 @@
+// /src/pages/EnseignantNotes.tsx
+// Page "Évaluations & Notes"
+
+import React from 'react';
+import EnseignantNotesView from '@/components/notes/EnseignantNotesView';
+
+export default function EnseignantNotes() {
+  return <EnseignantNotesView />;
+}
